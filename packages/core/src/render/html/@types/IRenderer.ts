7395c407a077272124d2b66ded8f05163cac209b
MasterDan <MihaylovDS@gmail.com>
2023-02-10 22:13:57 +0300

    removed irenderer completely

--- packages/core/src/render/html/@types/IRenderer.ts
+++
@@ -1,5 +0,0 @@
-import { IBinding } from './binding-target';
-
-export interface IHtmlRenderer {
-  renderInto(binding: IBinding): Promise<IBinding | undefined>;
-}
--- packages/core/src/render/html/components/list-renderer-html.ts
+++ packages/core/src/render/html/components/list-renderer-html.ts
@@ -3,19 +3,53 @@ import {
   listComponentName,
 } from '@core/components/builtIn/list.component';
 import { Component } from '@core/components/conmponent';
+import { HtmlRendererBase } from '@core/render/base/html-renderer-base';
+import { from, last, map, of, pairwise, startWith, switchMap } from 'rxjs';
+import { filter } from 'rxjs/internal/operators/filter';
+import { take } from 'rxjs/internal/operators/take';
 import { IBinding } from '../@types/binding-target';
-import { IHtmlRenderer } from '../@types/IRenderer';
+import { ComponentRendererHtml } from '../render';
 
-export class ListRendererHtml implements IHtmlRenderer {
+export class ListRendererHtml extends HtmlRendererBase {
   constructor(private component: Component<IListComponentProps>) {
+    super();
     if (component.name !== listComponentName) {
       throw new Error('Must provide list component');
     }
   }
 
-  renderInto(binding: IBinding): Promise<IBinding | undefined> {
+  renderInto(target: IBinding) {
     const content = this.component.getProp('content') ?? [];
+    const contentRender$ = from(content).pipe(
+      map((c): ComponentRendererHtml => new ComponentRendererHtml(c)),
+      startWith(null),
+      pairwise(),
+    );
 
-    throw new Error('Method not implemented.');
+    contentRender$.subscribe(([prev, curr]) => {
+      if (curr == null) {
+        return;
+      }
+      if (prev == null) {
+        curr.target$.val = target;
+        curr.render();
+      } else {
+        prev.nextTarget$.subscribe((at) => {
+          curr.target$.val = at;
+        });
+        curr.target$
+          .pipe(
+            filter((t) => t != null),
+            take(1),
+          )
+          .subscribe(() => {
+            curr.render();
+          });
+      }
+    });
+    return contentRender$.pipe(
+      last(),
+      switchMap(([_, lastRender]) => lastRender?.target$ ?? of(undefined)),
+    );
   }
 }
