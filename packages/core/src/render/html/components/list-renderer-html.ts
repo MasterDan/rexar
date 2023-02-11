ee37f3d01b1f7f72990be62117241513bf89a45d
MasterDan <MihaylovDS@gmail.com>
2023-02-11 16:02:00 +0300

    rewrite list renderer

--- packages/core/src/render/html/components/list-renderer-html.ts
+++ packages/core/src/render/html/components/list-renderer-html.ts
@@ -4,11 +4,11 @@ import {
 } from '@core/components/builtIn/list.component';
 import { Component } from '@core/components/conmponent';
 import { HtmlRendererBase } from '@core/render/base/html-renderer-base';
-import { from, last, map, of, pairwise, startWith, switchMap } from 'rxjs';
-import { filter } from 'rxjs/internal/operators/filter';
-import { take } from 'rxjs/internal/operators/take';
+import { from } from 'rxjs';
+import { takeUntil } from 'rxjs/internal/operators/takeUntil';
+import { Subject } from 'rxjs/internal/Subject';
 import { IBinding } from '../@types/binding-target';
-import { ComponentRendererHtml } from '../render';
+import { ComponentRendererHtml, render } from '../render';
 
 export class ListRendererHtml extends HtmlRendererBase {
   constructor(private component: Component<IListComponentProps>) {
@@ -18,38 +18,32 @@ export class ListRendererHtml extends HtmlRendererBase {
     }
   }
 
+  unbind$ = new Subject<void>();
+
   renderInto(target: IBinding) {
     const content = this.component.getProp('content') ?? [];
-    const contentRender$ = from(content).pipe(
-      map((c): ComponentRendererHtml => new ComponentRendererHtml(c)),
-      startWith(null),
-      pairwise(),
-    );
-
-    contentRender$.subscribe(([prev, curr]) => {
-      if (curr == null) {
-        return;
-      }
-      if (prev == null) {
-        curr.target$.val = target;
-        curr.render();
-      } else {
-        prev.nextTarget$.subscribe((at) => {
-          curr.target$.val = at;
-        });
-        curr.target$
-          .pipe(
-            filter((t) => t != null),
-            take(1),
-          )
-          .subscribe(() => {
-            curr.render();
+    const renderContent = async () => {
+      this.unbind$.next();
+      let renderer: ComponentRendererHtml | null = null;
+      // eslint-disable-next-line no-restricted-syntax
+      for (const component of content) {
+        if (renderer == null) {
+          // eslint-disable-next-line no-await-in-loop
+          renderer = await render(component, target);
+        } else {
+          // eslint-disable-next-line no-await-in-loop
+          const newRenderer: ComponentRendererHtml = await render(
+            component,
+            renderer.nextTarget$.val ?? target,
+          );
+          renderer.nextTarget$.pipe(takeUntil(this.unbind$)).subscribe((nt) => {
+            newRenderer.target$.val = nt;
           });
+          renderer = newRenderer;
+        }
       }
-    });
-    return contentRender$.pipe(
-      last(),
-      switchMap(([_, lastRender]) => lastRender?.target$ ?? of(undefined)),
-    );
+      return renderer?.nextTarget$.value ?? undefined;
+    };
+    return from(renderContent());
   }
 }
