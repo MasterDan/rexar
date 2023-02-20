a865b2617791b94981c0a26fc6f48c0aa825c64b
MasterDan <MihaylovDS@gmail.com>
2023-02-20 20:57:47 +0300

    definition

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -1,3 +1,7 @@
+import {
+  CustomComponent,
+  ICustomComponentDefinitionArgs,
+} from './builtIn/custom/custom-template-component';
 import { Component, IComponentDefinitionArgs, TData } from './component';
 
 export type ComponentDefinition<TProps extends TData = TData> = {
@@ -5,9 +9,17 @@ export type ComponentDefinition<TProps extends TData = TData> = {
   name?: string;
 };
 
+function shouldWeCreateCustomComponent<TProps extends TData>(
+  args: IComponentDefinitionArgs<TProps>,
+): args is ICustomComponentDefinitionArgs<TProps> {
+  return (args as ICustomComponentDefinitionArgs<TProps>).template != null;
+}
+
 export function defineComponent<TProps extends TData = TData>(
   args: IComponentDefinitionArgs<TProps> = {},
 ): ComponentDefinition<TProps> {
-  const create = () => new Component<TProps>(args);
+  const create = shouldWeCreateCustomComponent(args)
+    ? () => new CustomComponent<TProps>(args)
+    : () => new Component<TProps>(args);
   return { create, name: args.name };
 }
