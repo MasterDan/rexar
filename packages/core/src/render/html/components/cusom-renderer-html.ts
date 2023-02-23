5f953cbefc103b6bee4ae95cc723b843fb5a9920
MasterDan <MihaylovDS@gmail.com>
2023-02-23 16:12:58 +0300

    custom renderer

--- packages/core/src/render/html/components/cusom-renderer-html.ts
+++ packages/core/src/render/html/components/cusom-renderer-html.ts
@@ -1,6 +1,10 @@
 import { CustomComponent } from '@core/components/builtIn/custom/custom-template-component';
-import { Observable } from 'rxjs';
+import { listComponent } from '@core/components/builtIn/list.component';
+import { from, Observable, of, switchMap } from 'rxjs';
+import { container } from 'tsyringe';
+import { AnyComponent } from '../@types/any-component';
 import { IBinding } from '../@types/binding-target';
+import { IHtmlRenderer } from '../@types/IHtmlRenderer';
 import { HtmlRendererBase } from '../base/html-renderer-base';
 
 export class CustomRendererHtml extends HtmlRendererBase {
@@ -8,6 +12,37 @@ export class CustomRendererHtml extends HtmlRendererBase {
     if (!(this.component instanceof CustomComponent)) {
       throw new Error('Component should be custom');
     }
-    throw new Error('Method not implemented.');
+    const component = this.component as CustomComponent;
+    const r = async (): Promise<Observable<IBinding | undefined>> => {
+      let template!: AnyComponent[];
+      if (typeof component.template === 'string') {
+        const { parseHtml } = await import('@core/parsers/html');
+        template = await parseHtml(component.template);
+      } else {
+        template = component.template;
+      }
+      if (template.length === 0) {
+        return of(undefined);
+      }
+      if (template.length === 1) {
+        const [componentTemplate] = template;
+        const renderer = container.resolve<IHtmlRenderer>('IHtmlRenderer');
+        renderer.setComponent(componentTemplate);
+        renderer.target$.val = target;
+        await renderer.render();
+        return renderer.nextTarget$;
+      }
+      if (template.length > 1) {
+        const componentTemplate = listComponent.create();
+        componentTemplate.bindProp('content', template);
+        const renderer = container.resolve<IHtmlRenderer>('IHtmlRenderer');
+        renderer.setComponent(componentTemplate);
+        renderer.target$.val = target;
+        await renderer.render();
+        return renderer.nextTarget$;
+      }
+      return of(undefined);
+    };
+    return from(r()).pipe(switchMap((x) => x));
   }
 }
