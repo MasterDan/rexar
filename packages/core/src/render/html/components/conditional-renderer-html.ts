1a4ad5ba3d9d5f42901b57648d06daf3ed5f0202
MasterDan <MihaylovDS@gmail.com>
2023-05-19 20:52:48 +0300

    rewrite conditional component renderer

--- packages/core/src/render/html/components/conditional-renderer-html.ts
+++ packages/core/src/render/html/components/conditional-renderer-html.ts
@@ -1,7 +1,8 @@
 import { IConditionalComponentProps } from '@core/components/builtIn/conditional.component';
+import { dynamic } from '@core/components/builtIn/dynamic.component';
 import { ref$ } from '@core/reactivity/ref';
 import { from, map, Observable, of, switchMap, take } from 'rxjs';
-import { combineLatest } from 'rxjs/internal/observable/combineLatest';
+import { filter } from 'rxjs/internal/operators/filter';
 import { skipUntil } from 'rxjs/internal/operators/skipUntil';
 import { injectable } from 'tsyringe';
 import { IBinding } from '../@types/binding-target';
@@ -10,87 +11,65 @@ import { resolveRenderer } from '../tools';
 
 @injectable()
 export class ConditionalRendererHtml extends HtmlRendererBase<IConditionalComponentProps> {
+  innerDynamic = dynamic();
+
+  innerDynamicRenderer = resolveRenderer(this.innerDynamic);
+
   condition$ = ref$(this.component$.pipe(map((c) => c?.getProp('if$'))));
 
-  positiveRenderer$ = ref$(
-    combineLatest([
-      this.component$.pipe(map((c) => c?.getProp('ifTrue$'))),
-      this.target$,
-    ]).pipe(
-      map(([c, t]) => {
-        if (c == null || c.val == null || t == null) {
-          return undefined;
-        }
-        return resolveRenderer(c.val, t);
-      }),
-    ),
+  positiveComponent$ = ref$(
+    this.component$.pipe(switchMap((c) => c?.getProp('ifTrue$'))),
   );
 
-  negativeRenderer$ = ref$(
-    combineLatest([
-      this.component$.pipe(map((c) => c?.getProp('ifFalse$'))),
-      this.target$,
-    ]).pipe(
-      map(([c, t]) => {
-        if (c == null || c.val == null || t == null) {
-          return undefined;
-        }
-        return resolveRenderer(c.val, t);
-      }),
-    ),
+  negativeComponent$ = ref$(
+    this.component$.pipe(switchMap((c) => c?.getProp('ifFalse$'))),
   );
+
+  constructor() {
+    super();
+    this.target$
+      .pipe(filter((t): t is IBinding => t != null))
+      .subscribe((t) => {
+        this.innerDynamicRenderer.target$.val = t;
+      });
+  }
 
   renderInto(): Observable<IBinding | undefined> {
     const condition$ = this.component.getProp('if$');
 
-    const firstMount$ = condition$.pipe(
-      switchMap((c) => (c ? this.positiveRenderer$ : this.negativeRenderer$)),
-      switchMap((r) => {
-        if (r == null) {
-          return of(undefined);
-        }
-        const renderAsync = async () => {
-          await r.render();
-          return r.nextTarget$;
-        };
-        return from(renderAsync());
-      }),
+    if (condition$.val) {
+      if (this.positiveComponent$.val) {
+        this.innerDynamic.bindProp('component$', this.positiveComponent$.val);
+      }
+    } else if (this.negativeComponent$.val) {
+      this.innerDynamic.bindProp('component$', this.negativeComponent$.val);
+    }
+
+    const renderAsync = async () => {
+      await this.innerDynamicRenderer.render();
+      return this.innerDynamicRenderer.nextTarget$;
+    };
+
+    const firstMount$ = from(renderAsync()).pipe(
       switchMap((v) => (v == null ? of(undefined) : v)),
       take(1),
     );
 
-    condition$.pipe(skipUntil(firstMount$)).subscribe(async (condition) => {
-      const negative = this.negativeRenderer$.val;
-      const positive = this.positiveRenderer$.val;
-      if (condition) {
-        if (negative) {
-          await negative.unmount();
-        }
-        if (positive) {
-          await positive.render();
-        }
-      } else {
-        if (positive) {
-          await positive.unmount();
-        }
-        if (negative) {
-          await negative.render();
-        }
-      }
-    });
+    condition$
+      .pipe(
+        skipUntil(firstMount$),
+        switchMap((c) =>
+          c ? this.positiveComponent$ : this.negativeComponent$,
+        ),
+      )
+      .subscribe(async (component) => {
+        this.innerDynamic.bindProp('component$', component);
+      });
 
     return firstMount$;
   }
 
   async unmount(): Promise<void> {
-    if (this.condition$.val == null) {
-      return;
-    }
-    if (this.condition$.val && this.positiveRenderer$.val) {
-      await this.positiveRenderer$.val.unmount();
-    }
-    if (!this.condition$.val && this.negativeRenderer$.val) {
-      await this.negativeRenderer$.val.unmount();
-    }
+    this.innerDynamicRenderer.unmount();
   }
 }
