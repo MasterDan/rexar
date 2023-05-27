d3d53801bd4ebf81c4dd8535053836344ccb17c6
MasterDan <MihaylovDS@gmail.com>
2023-05-27 21:41:37 +0300

    rename

--- packages/example/src/components/input-number-test/input-number-test.component.ts
+++ packages/example/src/components/input-number-test/input-number-test.component.ts
@@ -10,13 +10,18 @@ import template from 'bundle-text:./input-number-test.component.html';
 export const inputNumberTest = defineComponent({
   template,
   setup() {
-    const num$ = ref$(2);
-    const num2$ = ref$(2);
-    bindNumericValue('#number', num$);
-    bindNumericValue('#number-two', num2$);
+    const numberOne$ = ref$(2);
+    const numberTwo$ = ref$(2);
+    bindNumericValue('#number', numberOne$);
+    bindNumericValue('#number-two', numberTwo$);
     innerTextFor(
       '#sum-text',
-      ref$(() => `${num$.val} + ${num2$.val} = ${num$.val + num2$.val}`),
+      ref$(
+        () =>
+          `${numberOne$.val} + ${numberTwo$.val} = ${
+            numberOne$.val + numberTwo$.val
+          }`,
+      ),
     );
   },
 });
