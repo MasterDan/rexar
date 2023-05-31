6c837317ee5f64cf76b23a24016bf12ae207ae56
MasterDan <MihaylovDS@gmail.com>
2023-05-31 21:24:38 +0300

    simplified method

--- packages/core/src/app/index.ts
+++ packages/core/src/app/index.ts
@@ -1,14 +1,14 @@
 import { AnyComponent } from '@core/render/html/@types/any-component';
 import { DocumentRef } from '@core/render/html/documentRef';
 import { container } from 'tsyringe';
-import { getAppRenderer } from '@core/render/html';
+import { resolveAplicationRenderer } from '@core/render/html';
 import { lastValueFrom } from 'rxjs';
 import { AnyComponentDefinition } from '@core/components';
 import { Component } from '@core/components/component';
 
 export function createApp(root: AnyComponent | AnyComponentDefinition) {
   const rootComponent = root instanceof Component ? root : root.create();
-  const renderer = getAppRenderer();
+  const renderer = resolveAplicationRenderer();
   const mount = async (selector: string) => {
     const doc = await lastValueFrom(container.resolve(DocumentRef).instance$);
     const el = doc.querySelector(selector);
