479f4e8cee1298815bbeb444a5e0fdff549ded79
MasterDan <MihaylovDS@gmail.com>
2023-06-05 22:21:43 +0300

    implemented repeat in demo

--- packages/example/src/components/input-text-test/input-text-test.component.ts
+++ packages/example/src/components/input-text-test/input-text-test.component.ts
@@ -3,20 +3,28 @@ import {
   bindTextContent,
   ref$,
   bindStringValue,
+  repeatTemplate,
 } from '@rexar/core';
 import template from 'bundle-text:./input-text-test.component.html';
 
 export const inputTextTest = defineComponent({
   template: () => template,
   setup() {
-    const textOne$ = ref$('hello');
+    const textOne$ = ref$('Hello');
     const textTwo$ = ref$('World');
     bindStringValue('one', textOne$);
     bindStringValue('one-second', textOne$);
     bindStringValue('two', textTwo$);
-    bindTextContent(
-      'text',
-      ref$(() => `${textOne$.value} ${textTwo$.value}`),
-    );
+    const fullText$ = ref$(() => `${textOne$.value}, ${textTwo$.value}`);
+    bindTextContent('text', fullText$);
+    repeatTemplate({
+      templateId: 'item-template',
+      array: ref$(() => fullText$.value.split('').filter((v) => v !== ' ')),
+      key: (i) => i,
+      setup: ({ props: itemProps }) => {
+        const letter$ = ref$(() => ` ${itemProps.item.value?.value ?? '-'}`);
+        bindTextContent('letter', letter$);
+      },
+    }).mount('letters');
   },
 });
