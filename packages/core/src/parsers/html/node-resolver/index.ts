145e260c9159a2d7728ca8d36556313e985dd607
MasterDan <MihaylovDS@gmail.com>
2023-06-17 23:30:17 +0300

    node-resolver depends on enviroment

--- packages/core/src/parsers/html/node-resolver/index.ts
+++ packages/core/src/parsers/html/node-resolver/index.ts
@@ -1,12 +1,10 @@
-import { isBrowser } from '@core/tools/env';
-
 export async function resolveNodes(html: string) {
-  if (isBrowser()) {
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(html, 'text/html');
-    return htmlDoc.body.childNodes;
+  if (process.env.NODE_ENV === 'test') {
+    const { JSDOM } = await import('jsdom');
+    const jd = new JSDOM(html);
+    return jd.window.document.body.childNodes;
   }
-  const { JSDOM } = await import('jsdom');
-  const jd = new JSDOM(html);
-  return jd.window.document.body.childNodes;
+  const parser = new DOMParser();
+  const htmlDoc = parser.parseFromString(html, 'text/html');
+  return htmlDoc.body.childNodes;
 }
