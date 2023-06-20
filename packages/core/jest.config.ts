04bedbdcd0369bb13d123546398a249bc9c9485c
MasterDan <MihaylovDS@gmail.com>
2023-06-20 22:19:13 +0300

    jest still broken

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -3,9 +3,10 @@ import type { Config } from '@jest/types';
 export default async (): Promise<Config.InitialOptions> => ({
   verbose: false,
   transform: {
-    '\\.[jt]s$': ['ts-jest', { useESM: true }],
+    '\\.[jt]s$': 'ts-jest',
     '^.+\\.html?$': 'jest-html-loader',
   },
+
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/$1',
   },
