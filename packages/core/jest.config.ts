79aa3e03f0075e4435753d09ec650c58bfde769b
MasterDan <MihaylovDS@gmail.com>
2023-06-24 00:55:00 +0300

    core tests works

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -3,11 +3,13 @@ import type { Config } from '@jest/types';
 export default async (): Promise<Config.InitialOptions> => ({
   verbose: false,
   transform: {
-    '\\.[jt]s$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.html?$': 'jest-html-loader',
   },
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/$1',
   },
+  transformIgnorePatterns: ['@rexar/reactivity'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 });
+
