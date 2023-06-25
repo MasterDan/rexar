a7d48fb8952ae76114b42a747ea29ae09e22f851
MasterDan <MihaylovDS@gmail.com>
2023-06-25 21:10:10 +0300

    reactivity test are verbose now

--- packages/reactivity/jest.config.ts
+++ packages/reactivity/jest.config.ts
@@ -3,7 +3,7 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const jestConfig: JestConfigWithTsJest = {
   // [...]
   extensionsToTreatAsEsm: ['.ts'],
-
+  verbose: true,
   moduleNameMapper: {
     '^@reactivity/(.*)$': '<rootDir>/src/$1',
     '^(\\.{1,2}/.*)\\.js$': '$1',
