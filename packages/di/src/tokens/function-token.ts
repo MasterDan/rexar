d49f7d7b714293b5f42eadb5299ec978c4ecbe73
MasterDan <MihaylovDS@gmail.com>
2023-06-29 21:43:04 +0300

    function injection works

--- packages/di/src/tokens/function-token.ts
+++ packages/di/src/tokens/function-token.ts
@@ -1,21 +1,65 @@
+import type { DiContainer } from '../container/di-container';
 import { AnyFnToken, IToken } from './@types/IToken';
+import { TokenOperator } from './@types/TokenOperator';
 
-export class FunctionToken<TArgs extends [], TResult>
-  implements IToken<(...args: TArgs) => TResult>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AnyAray = Array<any>;
+export class FunctionToken<
+  TArgs extends AnyAray,
+  TResult,
+  TInitialArgs extends AnyAray = TArgs,
+> implements
+    IToken<(...args: TInitialArgs) => TResult, (...args: TArgs) => TResult>
 {
   key: symbol;
 
   name: string;
 
-  constructor(private token: AnyFnToken) {
+  constructor(
+    private token: AnyFnToken,
+    private fnResolve?: (
+      fn: (...args: TArgs) => TResult,
+    ) => (...fnargs: TInitialArgs) => TResult,
+  ) {
     this.key = token.key;
     this.name = token.name;
   }
-  provide(resolver: (...args: TArgs) => TResult): void {
-    throw new Error('Method not implemented.');
+
+  provide(fn: (...args: TArgs) => TResult): void {
+    this.token.provide(() => fn);
   }
 
-  resolve(): (...args: TArgs) => TResult {
-    throw new Error('Method not implemented.');
+  resolve() {
+    const func = this.token.resolve() as (...args: TArgs) => TResult;
+    return this.fnResolve
+      ? this.fnResolve(func)
+      : (func as unknown as (...args: TInitialArgs) => TResult);
   }
+}
+
+export function useFunction<
+  TResult,
+  TArgs extends AnyAray,
+  TInitialArgs extends AnyAray = TArgs,
+>(
+  transformArgs?: (container: DiContainer, ...args: TInitialArgs) => TArgs,
+): TokenOperator<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  any,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  () => any,
+  (...args: TInitialArgs) => TResult,
+  (...args: TArgs) => TResult
+> {
+  return (token, container) => {
+    const func = (fn: (...args: TArgs) => TResult) =>
+      transformArgs == null
+        ? (...fnargs: TInitialArgs) => fn(...(fnargs as unknown as TArgs))
+        : (...fnargs: TInitialArgs) => {
+            const iargs = transformArgs(container, ...fnargs);
+            return fn(...iargs);
+          };
+    return new FunctionToken<TArgs, TResult, TInitialArgs>(token, func);
+    throw new Error('Not Implemented!');
+  };
 }
--- packages/di/src/tokens/index.spec.ts
+++ packages/di/src/tokens/index.spec.ts
@@ -1,4 +1,5 @@
 import { container } from '../container/di-container';
+import { useFunction } from './function-token';
 import { useValue } from './value-token';
 
 describe('dependecy injection', () => {
@@ -9,5 +10,29 @@ describe('dependecy injection', () => {
     const valueCopy = container.resolve<number>('value');
     expect(value).toBe(5);
     expect(valueCopy).toBe(5);
+  });
+  test('function injection', () => {
+    const valueToken = container.createToken('value', useValue<number>());
+    valueToken.provide(5);
+
+    const functionToken = container.createToken(
+      'func',
+      useFunction<number, [number, number], [number]>((c, first) => [
+        first,
+        c.resolve<number>('value'),
+      ]),
+    );
+    functionToken.provide((x, y) => x + y);
+    const addValue = functionToken.resolve();
+    expect(addValue(2)).toBe(7);
+
+    const simpleFunctionToken = container.createToken(
+      'simple-func',
+      useFunction<number, [number, number]>(),
+    );
+    simpleFunctionToken.provide((x, y) => x * y);
+    const multi =
+      container.resolve<(x: number, y: number) => number>('simple-func');
+    expect(multi(2, 3)).toBe(6);
   });
 });
