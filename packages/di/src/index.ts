c7a400e7e7ed69a424fcfed242aca7db01c2f6da
MasterDan <MihaylovDS@gmail.com>
2023-07-01 17:19:00 +0300

    package build

--- packages/di/src/index.ts
+++ packages/di/src/index.ts
@@ -1,3 +1,6 @@
-export function foo() {
-  return 'foo';
-}
+export { container, DiContainer } from './container/di-container';
+export { useValue } from './tokens/value.token';
+export { useFunction } from './tokens/function.token';
+export { useClass } from './tokens/class.token';
+export { singleton } from './tokens/singleton.token';
+export { lazy } from './tokens/lazy.token';
