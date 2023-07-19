73e88244ef66b8c3baa979de0e07e233176243bc
MasterDan <MihaylovDS@gmail.com>
2023-07-19 18:09:23 +0300

    removed parcel and jest everywhere

--- packages/di/src/index.spec.ts
+++ packages/di/src/index.spec.ts
@@ -1,4 +1,5 @@
 /* eslint-disable max-classes-per-file */
+import { describe, test, expect } from 'vitest';
 import { container } from './container/di-container';
 import { useClass } from './tokens/class.token';
 import { useFunction } from './tokens/function.token';
