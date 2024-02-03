a00aa523a6d2705141683e2c437125d11620cb94
MasterDan <MihaylovDS@gmail.com>
2024-02-03 19:05:13 +0300

    async importing script

--- packages/docs/components/en/reactivity/observable/Observables.setup.ts
+++ packages/docs/components/en/reactivity/observable/Observables.setup.ts
@@ -1,14 +1,15 @@
 import { skip, take, timer } from 'rxjs';
 
-export function setup() {
-  const el = document.querySelector('div#observables');
-  const counterObservable = timer(0, 1000).pipe(skip(1), take(9));
-  counterObservable.subscribe({
-    next: (counter) => {
-      el!.innerHTML = `Counter is ${counter}`;
-    },
-    complete: () => {
-      el!.innerHTML = `We successfully counted to 10`;
-    },
-  });
+const el = document.querySelector('div#observables');
+if (el === null) {
+  throw new Error('Could not find div#observables');
 }
+const counterObservable = timer(0, 1000).pipe(skip(1), take(9));
+counterObservable.subscribe({
+  next: (counter) => {
+    el.innerHTML = `Counter is ${counter}`;
+  },
+  complete: () => {
+    el.innerHTML = `We successfully counted to 10`;
+  },
+});
--- packages/docs/components/en/reactivity/observable/Observables.vue
+++ packages/docs/components/en/reactivity/observable/Observables.vue
@@ -1,9 +1,8 @@
 <template src="./Observables.template.html"></template>
 <script setup>
 import { onMounted } from 'vue';
-import { setup } from './Observables.setup';
 
 onMounted(() => {
-  setup();
+  import('./Observables.setup');
 });
 </script>
