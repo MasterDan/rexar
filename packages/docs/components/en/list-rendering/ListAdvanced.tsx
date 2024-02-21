870ec6ca916694a19ffb2e87fe15f1ca88a8d1d9
MasterDan <MihaylovDS@gmail.com>
2024-02-21 22:25:16 +0300

    simplified list

--- packages/docs/components/en/list-rendering/ListAdvanced.tsx
+++ packages/docs/components/en/list-rendering/ListAdvanced.tsx
@@ -6,12 +6,12 @@ import {
   fragment,
   computed,
   useIf,
-} from "@rexar/core";
-import { take, timer } from "rxjs";
+} from '@rexar/core';
+import { take, timer } from 'rxjs';
 
 export const List = defineComponent(() => {
   const array = ref<number[]>([]);
-  timer(0, 1000)
+  timer(1000, 1000)
     .pipe(take(6))
     .subscribe((i) => {
       array.value.push(i + 1);
@@ -44,49 +44,44 @@ export const List = defineComponent(() => {
             <tr>
               <th>Index</th>
               <th>Value</th>
-              <th style={{ textAlign: "center" }} colSpan={3}>
+              <th style={{ textAlign: 'center' }} colSpan={3}>
                 Actions
               </th>
             </tr>
           </thead>
           <tbody>
             <Numbers
-              each={({ item, index }) => {
-                const start = computed(() => index.value === 0);
-                const end = computed(
-                  () => index.value === array.value.length - 1
-                );
-
-                return (
-                  <tr>
-                    <td style={{ textAlign: "right" }}>{index}</td>
-                    <td style={{ textAlign: "right" }}>{item}</td>
-                    <td>
-                      <button
-                        disabled={start}
-                        onClick={() => moveUp(index.value)}
-                      >
-                        ▲
-                      </button>
-                    </td>
-                    <td>
-                      <button
-                        disabled={end}
-                        onClick={() => moveDown(index.value)}
-                      >
-                        ▼
-                      </button>
-                    </td>
-                    <td>
-                      <button onClick={() => remove(index.value)}>✕</button>
-                    </td>
-                  </tr>
-                );
-              }}
+              each={({ item, index }) => (
+                <tr>
+                  <td style={{ textAlign: 'right' }}>{index}</td>
+                  <td style={{ textAlign: 'right' }}>{item}</td>
+                  <td>
+                    <button
+                      disabled={() => index.value === 0}
+                      onClick={() => moveUp(index.value)}
+                    >
+                      ▲
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      disabled={() => index.value === array.value.length - 1}
+                      onClick={() => moveDown(index.value)}
+                    >
+                      ▼
+                    </button>
+                  </td>
+                  <td>
+                    <button onClick={() => remove(index.value)}>✕</button>
+                  </td>
+                </tr>
+              )}
             />
           </tbody>
         </table>
-        <button onClick={reverse}>Reverse</button>
+        <button disabled={() => array.value.length < 2} onClick={reverse}>
+          Reverse
+        </button>
       </NotEmpty>
       <Empty>No items</Empty>
     </>
