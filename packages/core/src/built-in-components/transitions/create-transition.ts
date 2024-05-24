dedf91eccff99a47f548f28269b99f3653e25066
MasterDan <MihaylovDS@gmail.com>
2024-05-24 18:24:02 +0300

    attach method

--- packages/core/src/built-in-components/transitions/create-transition.ts
+++ packages/core/src/built-in-components/transitions/create-transition.ts
@@ -1,5 +1,19 @@
-import { ref } from '@rexar/reactivity';
+import {
+  ValueOrObservableOrGetter,
+  ref,
+  toObservable,
+} from '@rexar/reactivity';
 import { StyleAttributes } from '@rexar/jsx';
+import {
+  Observable,
+  combineLatestWith,
+  filter,
+  map,
+  pairwise,
+  startWith,
+  switchMap,
+} from 'rxjs';
+import { AnyRecord } from '@rexar/tools';
 
 export type TransitionStateKeys<TAdditionalKeys = 'default' | 'void'> =
   | 'default'
@@ -16,20 +30,43 @@ export type TransitionState =
       style: StyleAttributes;
     };
 
+function applyState(state: TransitionState, el: HTMLElement) {
+  if (state.type === 'class') {
+    el.classList.add(state.class);
+  } else if (state.type === 'style') {
+    Object.entries(state.style).forEach(([key, value]) => {
+      if (key in el.style) {
+        (el.style as AnyRecord)[key] = value;
+      }
+    });
+  }
+}
+function removeState(state: TransitionState, el: HTMLElement) {
+  if (state.type === 'class') {
+    el.classList.remove(state.class);
+  } else if (state.type === 'style') {
+    Object.keys(state.style).forEach((key) => {
+      if (key in el.style) {
+        (el.style as AnyRecord)[key] = undefined;
+      }
+    });
+  }
+}
+
 class Transition<TStates extends string> {
-  states$ = ref(new Map<TransitionStateKeys<TStates>, TransitionState>());
+  states = new Map<TransitionStateKeys<TStates>, TransitionState>();
 
-  transitions$ = ref(new Map<string, TransitionState>());
+  transitions = new Map<string, TransitionState>();
 
-  currentState$ = ref<TransitionStateKeys<TStates>>('default');
+  defaultState: TransitionStateKeys<TStates> = 'default';
 
   setState<T extends TransitionStateKeys<TStates>>(state: T) {
     const withClass = (className: string): Transition<TStates> => {
-      this.states$.value.set(state, { type: 'class', class: className });
+      this.states.set(state, { type: 'class', class: className });
       return this;
     };
     const withStyle = (style: StyleAttributes): Transition<TStates> => {
-      this.states$.value.set(state, { type: 'style', style });
+      this.states.set(state, { type: 'style', style });
       return this;
     };
     return { withClass, withStyle };
@@ -45,14 +82,14 @@ class Transition<TStates extends string> {
       const to = (toKey: TransitionStateKeys<TStates> | '*') => {
         const transitionKey = `${fromKey}=>${toKey}`;
         const withClass = (className: string): Transition<TStates> => {
-          this.transitions$.value.set(transitionKey, {
+          this.transitions.set(transitionKey, {
             type: 'class',
             class: className,
           });
           return this;
         };
         const withStyle = (style: StyleAttributes): Transition<TStates> => {
-          this.transitions$.value.set(transitionKey, { type: 'style', style });
+          this.transitions.set(transitionKey, { type: 'style', style });
           return this;
         };
         return { withClass, withStyle };
@@ -63,8 +100,62 @@ class Transition<TStates extends string> {
   }
 
   withInitialSate(state: TransitionStateKeys<TStates>) {
-    this.currentState$.value = state;
+    this.defaultState = state;
     return this;
+  }
+
+  attachTo(el: ValueOrObservableOrGetter<HTMLElement | undefined>) {
+    const el$ = toObservable(el).pipe(filter((v) => v != null));
+    const processing$ = ref(false);
+    const state$ = ref<TransitionStateKeys<TStates>>(this.defaultState);
+
+    const setState = (state: TransitionStateKeys<TStates>) => {
+      state$.value = state;
+    };
+
+    const statePairwise$ = state$.pipe(
+      startWith('*'),
+      pairwise(),
+    ) as unknown as Observable<
+      [TransitionStateKeys<TStates> | '*', TransitionStateKeys<TStates> | '*']
+    >;
+
+    processing$
+      .pipe(
+        filter((v) => !v),
+        switchMap(() => statePairwise$),
+        map(([from, to]) => ({ from, to })),
+        combineLatestWith(el$.pipe(filter((v): v is HTMLElement => v != null))),
+      )
+      .subscribe(([{ from, to }, element]) => {
+        processing$.value = true;
+        const fromState = from === '*' ? undefined : this.states.get(from);
+        const toState = to === '*' ? undefined : this.states.get(to);
+        const transition = this.transitions.get(`${from}=>${to}`);
+
+        if (transition != null) {
+          applyState(transition, element);
+        }
+        if (toState != null) {
+          applyState(toState, element);
+        }
+
+        const removeFromState = () => {
+          if (fromState != null) {
+            removeState(fromState, element);
+          }
+          if (transition != null) {
+            removeState(transition, element);
+          }
+          element.removeEventListener('transitionend', removeFromState);
+          processing$.value = false;
+        };
+        element.addEventListener('transitionend', removeFromState);
+      });
+
+    return {
+      setState,
+    };
   }
 }
 
